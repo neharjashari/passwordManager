9c5dd4bd13d834067d3e3aa875db73ae0ab89beb
neharjashari <neharjashari@gmail.com>
2019-07-09 10:13:43 +0200

    Commiting the project in GitHub

---
+++ passwordManager/SignUpViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewViewController.swift
+//  passwordManager
+//
+//  Created by Nehar Jashari on 7/9/19.
+//  Copyright © 2019 Nehar Jashari. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ passwordManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passwordManager
+//
+//  Created by Nehar Jashari on 7/9/19.
+//  Copyright © 2019 Nehar Jashari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
