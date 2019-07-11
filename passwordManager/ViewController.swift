d981ed27af8939bba8c5b984196ea66cbd695c63
neharjashari <neharjashari@gmail.com>
2019-07-11 19:33:26 +0200

    Design of Login view and SignUp view

--- passwordManager/ViewController.swift
+++ passwordManager/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    // on click event for the Login Button
+    @IBAction func onClickLogin(_ sender: UIButton) {
+        // the sender is the object that called this method
+        
+    }
+    
+    // if don't have account go to sign up view to create one
+    @IBAction func goToSignUp(_ sender: Any) {
+        
+    }
 }
 
